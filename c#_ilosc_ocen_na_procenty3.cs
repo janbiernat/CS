598ead93068e91366464b97c1e5758859c803118
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-07-07 21:59:21 +0200

    Add files via upload

---
+++ c#_ilosc_ocen_na_procenty3.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*--== % udział poszczególnych ocen w całkowitej ilości ocen 3 ==--
+  Copyright (c)by Jan T. Biernat
+*/
+namespace OcenyNaProcent3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--== % udział poszczególnych ocen w całkowitej ilości ocen 3 ==--");
+            Console.WriteLine("Copyright (c)by Jan T. Biernat \n \n");
+            //
+            int Suma = 0;
+            int[] Oceny = new int[6];
+            float Procent = 0;
+            //
+            //Pobieranie danych od użytkownika.
+            for (int I = 0; I < 6; I++)
+            {
+                Console.Write("Ocena ");
+                Console.Write((6 - I));
+                Console.Write(": ");
+                Oceny[I] = 0;
+                while(int.TryParse(Console.ReadLine(), out Oceny[I]) != true) {
+                    Console.WriteLine("BŁĄD -?To nie jest liczba! \n");
+                    Console.Write("Ocena ");
+                    Console.Write((6 - I));
+                    Console.Write(": ");
+                }
+                if (Oceny[I] < 0) { Oceny[I] = 0; }
+                Suma += Oceny[I];
+            }
+            //
+            //Raport: Udział procentowy poszczególnych ocen.
+            if (Suma > 0)
+            {
+                Console.Write("\nSuma ocen: ");
+                Console.Write(Suma);
+                Console.WriteLine("\n \n Ocena | Ilość |     %");
+                Console.WriteLine(new string('-', 25));
+                for (int I = 0; I < 6; I++)
+                {
+                    Console.Write((6 - I).ToString().PadLeft(6));
+                    Console.Write(" | ");
+                    Console.Write(Oceny[I].ToString().PadLeft(5));
+                    Console.Write(" | ");
+                    Procent = 0; Procent = (Oceny[I] * 100f / Suma);
+                    Console.Write(Procent.ToString("F2").PadLeft(6));
+                    Console.Write("\n");
+                }
+            }
+            //
+            //Naciśnij dowolny klawisz.
+            Console.Write("\n \nNaciśnij dowolny klawisz...");
+            Console.ReadKey();
+        }
+    }
+}
